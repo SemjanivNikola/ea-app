a531d0f10034adaf51154d901c2456722b73b05b
SemjanivNikola <nikola.sem450@gmail.com>
2023-07-26 11:08:41 +0200

    Update App.tsx for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,20 @@
-import { useState } from "react";
-import { useSpeechRecognition } from "react-speech-kit";
+// import { useSpeechRecognition } from "react-speech-kit";
 import "./App.css";
+import CreateDocumentScreen from "./features/create-document/CreateDocumentScreen";
 
 function App() {
-    const [value, setValue] = useState("");
-    const { listen, listening, stop } = useSpeechRecognition({
-        onResult: (result: string) => {
-            setValue(result);
-        }
-    });
+    // const [value, setValue] = useState("");
+    // const { listen, listening, stop } = useSpeechRecognition({
+    //     onResult: (result: string) => {
+    //         setValue(result);
+    //     }
+    // });
 
-    function toggle() {
-        listen({ continuous: true });
-    }
+    // function toggle() {
+    //     listen({ continuous: true });
+    // }
 
-    return (
-        <div>
-            <textarea value={value} onChange={(event) => setValue(event.target.value)} />
-            <button onMouseDown={listen} onMouseUp={stop}>
-                🎤
-            </button>
-            <button onClick={toggle}>🎤 toggle listening</button>
-            {listening && <div>Go ahead I'm listening</div>}
-        </div>
-    );
+    return <CreateDocumentScreen />;
 }
 
 export default App;
