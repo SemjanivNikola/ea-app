1cba7da2700ce03bc4d4b3271849fa30f04000ce
SemjanivNikola <nikola.sem450@gmail.com>
2023-07-29 17:35:42 +0200

    Update prop names on worker

--- src/features/create-edit-document/visualizer.worker.ts
+++ src/features/create-edit-document/visualizer.worker.ts
@@ -7,31 +7,34 @@ type DrawVisualizerProps = Pick<VisualizerProps, "dataArray">;
 
 let canvas: OffscreenCanvas | null = null;
 
-const meterWidth = 8;
+const barWidth = 8;
 const gap = 2;
 
 const drawVisualizer = ({ dataArray }: DrawVisualizerProps) => {
     if (canvas) {
         const cwidth = canvas.width;
         const cheight = canvas.height;
-        const meterNum = cwidth / (meterWidth + gap);
+        const barNum = cwidth / (barWidth + gap);
         const ctx = canvas.getContext("2d")!;
+
         const gradient = ctx.createLinearGradient(0, 0, 0, cheight);
         gradient.addColorStop(1, "#8968bc");
         gradient.addColorStop(0.7, "#71bc68");
         gradient.addColorStop(0, "#bc689b");
+
         ctx.fillStyle = gradient;
 
         const draw = () => {
-            const step = Math.round(dataArray.length / meterNum);
+            const step = Math.round(dataArray.length / barNum);
 
             // Clear previous state, so we get actuall animation
             ctx.clearRect(0, 0, cwidth, cheight);
 
-            for (let i = 0; i < meterNum; i++) {
+            for (let i = 0; i < barNum; i++) {
                 const value = dataArray[i * step];
 
-                ctx.fillRect(i * (meterWidth + gap), cheight - value, meterWidth, cheight);
+                // Divide height with value to display audio as graph on bottom on clear background
+                ctx.fillRect(i * (barWidth + gap), cheight - value, barWidth, cheight);
             }
         };
         requestAnimationFrame(draw);
